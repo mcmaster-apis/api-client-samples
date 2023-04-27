fa8797125270e38d065d079ed74c39080cf26fcc
David Arnold <arnoldd@mcmaster.ca>
2023-04-27 15:04:22 -0400

    Cooridnate calendar endpoint url change

--- academic-calendar/react/public/environment-template.js
+++ academic-calendar/react/public/environment-template.js
@@ -4,7 +4,7 @@
 
 window._env = {
   calendarApi: {
-    endpoint: "https://api.mcmaster.ca/calendar/v2",
+    endpoint: "https://api.mcmaster.ca/academic-calendar/v2",
     revision: "latest", // Unless otherwise documented, your choices are "latest" or "stable" (the default)
     key: "" // You will need a product subscription to obtain an API key
   }
